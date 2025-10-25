dc5251373e19da66868d421ce592b5adc1943449
Rajeshwari1403 <rajeshwariravi2005@gmail.com>
2025-10-25 21:43:38 +0530

    yes

--- src/section/MyProjects.jsx
+++ src/section/MyProjects.jsx
@@ -22,7 +22,7 @@ const MyProjects = () => {
   }, [emblaApi, updateScrollButtons]);
 
   return (
-    <section id='projects' className='bg-primary mt-[-16px] mx-auto max-w-8xl rounded-lg scroll-mt-20'>
+    <section id='projects' className='bg-primary mt-[-16px] mx-auto max-w-7xl rounded-lg scroll-mt-20'>
       <div className='container mx-auto px-8 md:px-10 py-10'>
         <div className='w-full lg:w-[60vw] mx-auto'>
           <h4 className='sec-title mt-[-10px]'>Projects</h4>
